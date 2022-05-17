ca1c42c82bddefde8462fe3538a8e597046a6c0a
taras.lishchenko <taras.lishchenko@gmail.com>
2022-05-17 14:30:02 +0300

    works with xlsx

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-import csv
+from collections import defaultdict
+import typing as typ
+import io
 from copy import deepcopy
+from dataclasses import dataclass
 from datetime import datetime
 
 import pandas as pd
 import streamlit as st
 
 
-st.set_page_config(layout="wide")
-
-
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
-local_css("style.css")
-
 
 class VehicleJournalTable:
-    ID = "id"
+    ID = "№"
     VEHICLE_MODEL = "марка машини"
     LICENCE_PLATE = "номерний знак"
     GROUP_OF_OPERATION = "група експлуатації"
@@ -27,113 +25,263 @@ class VehicleJournalTable:
     TIME_CHECK_OUT = "час виїзду"
     TIME_CHECK_IN = "час повернення"
 
-    @classmethod
-    def items(cls):
-        return [
-            cls.ID, cls.VEHICLE_MODEL,
-            cls.LICENCE_PLATE, cls.GROUP_OF_OPERATION,
-            cls.VEHICLE_PURPOSE, cls.ROUTE,
-            cls.RESPONSIBLE,
-            cls.TIME_CHECK_OUT, cls.TIME_CHECK_IN
-        ]
 
-    @classmethod
-    def sizes(cls):
-        return
-
-
-    @classmethod
-    def num_columns(cls):
-        return len(cls.items())
-
-
-class Buttons:
+class Controls:
     CHECK_OUT = "Виїхала"
     CHECK_IN = "Повернулась"
 
+    HEADER = "Транспортні засоби"
+    UPLOAD_FILE = "Обрати файл"
+    DOWNLOAD = "Завантажити (журнал)"
+    CLEAR_ALL = "Очистити (все)"
+    CLEAR_CHECKED_IN = "Очистити (повернулись)"
 
-columns = [
-    VehicleJournalTable.ID,
-    VehicleJournalTable.VEHICLE_MODEL,
-    VehicleJournalTable.LICENCE_PLATE,
-    VehicleJournalTable.GROUP_OF_OPERATION,
-    VehicleJournalTable.VEHICLE_PURPOSE,
-    VehicleJournalTable.ROUTE,
-    VehicleJournalTable.RESPONSIBLE,
-    Buttons.CHECK_OUT,
-    VehicleJournalTable.TIME_CHECK_OUT,
-    Buttons.CHECK_IN,
-    VehicleJournalTable.TIME_CHECK_IN,
-]
 
-indexes = {c:i for i, c in enumerate(columns)}
+@dataclass
+class VehicleLogItem:
+    _check_in_time: datetime = None
+    _check_out_time: datetime = None
 
-containers_size = [1] * len(columns)
+    def check_in(self, time: datetime = None):
+        self._check_in_time = time or datetime.now()
+        return self
 
-if 'events' not in st.session_state:
-    st.session_state['events'] = []
+    def check_out(self, time: datetime = None):
+        self._check_out_time = time or datetime.now()
+        return self
 
-events = st.session_state['events']
-vehicles = {}
+    @property
+    def check_in_time(self):
+        return self._check_in_time
 
-st.header("Транспортні засоби")
-with open("vehicles.csv", "r", newline='') as csv_file:
-    reader = csv.DictReader(csv_file)
-    containers = st.columns(containers_size)
-    for i, column in enumerate(columns):
+    @property
+    def check_out_time(self):
+        return self._check_out_time
+
+    @property
+    def checked_in(self):
+        return isinstance(self._check_in_time, datetime)
+
+
+class VehicleLogs:
+
+    def __init__(self) -> None:
+        self._logs: typ.List[VehicleLogItem] = list()
+
+    def check_in(self, time: datetime = None):
+        if self.last:
+            self.last.check_in(time)
+
+    def check_out(self, time: datetime = None):
+        if self.last and not self.last.checked_in:
+            self.last.check_in(time)
+
+        self._logs.append(VehicleLogItem().check_out(time))
+
+    def clear_checked_in(self):
+        logs = [self.last] if self.last else []
+        self._logs = logs + [l for l in self._logs[:-1] if not l.checked_in]
+
+    def clear(self):
+        self._logs = [self.last] if self.last else []
+
+    @property
+    def check_in_time(self):
+        return self.last.check_in_time if self.last else None
+
+    @property
+    def check_out_time(self):
+        return self.last.check_out_time if self.last else None
+
+    @property
+    def checked_in(self):
+        return self.last.checked_in if self.last else True
+
+    @property
+    def last(self):
+        return self._logs[-1] if len(self._logs) > 0 else None
+
+    def __len__(self):
+        return len(self._logs)
+
+    def __iter__(self):
+        for l in self._logs:
+            yield l
+
+
+def main():
+    datetime_format = "%H:%M:%S %d.%m.%Y"
+
+    st.set_page_config(layout="wide")
+
+    local_css("style.css")
+
+    # load events from history (cache)
+    if 'events' not in st.session_state:
+        st.session_state['events'] = defaultdict(VehicleLogs)
+    events = st.session_state['events']
+
+    # column size for `ui element`
+    ui_elements = {
+        Controls.HEADER: 5,
+        Controls.DOWNLOAD: 1,
+        Controls.CLEAR_ALL: 1,
+        Controls.CLEAR_CHECKED_IN: 1,
+    }
+
+    # prepare containers
+    ordered_elements = [Controls.HEADER,
+                        Controls.DOWNLOAD,
+                        Controls.CLEAR_ALL, Controls.CLEAR_CHECKED_IN]
+    elements_indexes = {elem: i for i, elem in enumerate(ordered_elements)}
+    ctrl_containers = st.columns([ui_elements[elem] for elem in ordered_elements])
+
+    # header title
+    elem_name = Controls.HEADER
+    ctrl_containers[elements_indexes[elem_name]].header(elem_name)
+
+    # clear all button
+    elem_name = Controls.CLEAR_ALL
+    if ctrl_containers[elements_indexes[elem_name]].button(elem_name):
+        for e in events.values():
+            e.clear()
+
+    # clear (checked-in) button
+    elem_name = Controls.CLEAR_CHECKED_IN
+    if ctrl_containers[elements_indexes[elem_name]].button(elem_name):
+        for e in events.values():
+            e.clear_checked_in()
+
+    # read vehicles from file
+    uploaded_file = st.sidebar.file_uploader(Controls.UPLOAD_FILE, type="xlsx")
+    if uploaded_file:
+        vehicles = pd.read_excel(uploaded_file,
+                                 converters={VehicleJournalTable.ID: int})
+
+        vehicles.set_index(VehicleJournalTable.ID)
+    else:
+        return
+
+
+    skip_columns = set([VehicleJournalTable.GROUP_OF_OPERATION,
+                        VehicleJournalTable.VEHICLE_PURPOSE])
+
+    # add columns for state (`check_in`, `check_out`)
+    data_columns = list(vehicles.columns)
+    short_data_columns = [c for c in data_columns if c not in skip_columns]
+    for i, (column, control) in enumerate(
+            zip([VehicleJournalTable.TIME_CHECK_OUT,
+                VehicleJournalTable.TIME_CHECK_IN],
+                [Controls.CHECK_OUT, Controls.CHECK_IN])):
+        vehicles.insert(list(vehicles.columns).index(column),
+                        control, [bool(i)] * len(vehicles))
+
+    display_columns = [c for c in vehicles.columns if c not in skip_columns]
+
+    # print header of table
+    sizes = [1] + [3] * (len(display_columns) - 1)
+    containers = st.columns(sizes)
+    for i, column in enumerate(display_columns):
         containers[i].write(column.capitalize())
     st.markdown("""---""")
 
-    datetime_format = "%H:%M:%S %d.%m.%Y"
-    for row in reader:
-        containers = st.columns(containers_size)
-        idx = row[VehicleJournalTable.ID]
+    # print table's rows
+    containers = st.columns(sizes)
+    indexes = {c:i for i, c in enumerate(display_columns)}
 
-        vehicle_present = True
-        i = indexes[Buttons.CHECK_OUT]
-        if containers[i].button(Buttons.CHECK_OUT, key=f"{Buttons.CHECK_OUT}:{idx}"):
-            vehicle_present = False
-            events.append([idx, datetime.now().strftime(datetime_format), None])
+    vehicles_data = vehicles[short_data_columns]
+    for idx, row in vehicles_data.iterrows():
+        containers = st.columns(sizes)
 
-        i = indexes[Buttons.CHECK_IN]
-        if containers[i].button(Buttons.CHECK_IN, key=f"{Buttons.CHECK_IN}:{idx}"):
-            vehicle_present = True
-            assert len(events) > 0
-            target_event = events[-1]
-            for i in range(len(events) - 1, 0):
-                if events[i][0] == idx:
-                    target_event = events[i]
-                    break
-            target_event[-1] = datetime.now().strftime(datetime_format)
+        # check-out button p
+        column_type = Controls.CHECK_OUT
+        if containers[indexes[column_type]].button(column_type,
+                                               key=f"{column_type}:{idx}"):
+            events[idx].check_out()
 
-        for column in VehicleJournalTable.items():
-            i = indexes[column]
-            if column == VehicleJournalTable.VEHICLE_MODEL:
-                color = 'green' if vehicle_present else 'red'
-                t = f"<div><span class='highlight {color}'>{row[column]}</span> </div>"
-                containers[i].markdown(t, unsafe_allow_html=True)
-            else:
-                containers[i].write(row[column])
+        column_type = Controls.CHECK_IN
+        if containers[indexes[column_type]].button(column_type,
+                                               key=f"{column_type}:{idx}"):
+            events[idx].check_in()
 
-        vehicles[idx] = [row[c] for c in VehicleJournalTable.items()]
+        color = 'green' if events[idx].checked_in else 'red'
+        for col in vehicles_data.columns:
+            text = row[col]
+            if col == VehicleJournalTable.TIME_CHECK_IN:
+                text = "_"
+                if events[idx].check_in_time:
+                    text = events[idx].check_in_time.strftime(datetime_format)
+                    text = f"<div><span class='highlight {color}'>{text}</span> </div>"
+            elif col == VehicleJournalTable.TIME_CHECK_OUT:
+                text = "_"
+                if events[idx].check_out_time:
+                    text = events[idx].check_out_time.strftime(datetime_format)
+                    if not events[idx].checked_in:
+                        text = f"<div><span class='highlight {color}'>{text}</span> </div>"
+
+            if len(events[idx]) > 0:
+                # we highlight only specific columns
+                if col not in [Controls.CHECK_IN,
+                               Controls.CHECK_OUT,
+                               VehicleJournalTable.TIME_CHECK_OUT,
+                               VehicleJournalTable.TIME_CHECK_IN]:
+                    text = f"<div><span class='highlight {color}'>{text}</span> </div>"
+
+            containers[indexes[col]].markdown(text, unsafe_allow_html=True)
+
         st.markdown("""---""")
 
+    st.header("Журнал")
 
-full_events = []
-for e in events:
-    vehicle_info = deepcopy(vehicles[e[0]])
-    vehicle_info[-1] = e[-1]
-    vehicle_info[-2] = e[-2]
-    full_events.append(vehicle_info)
+    # convert `events` to dataframe
+    events_df = []
+    for idx, row in vehicles[data_columns].iterrows():
+        logs = events[idx]
+        for record in logs:
+            info = deepcopy(row)
+            info[VehicleJournalTable.TIME_CHECK_IN] = "N/A"
+            info[VehicleJournalTable.TIME_CHECK_OUT] = "N/A"
+            if isinstance(record.check_in_time, datetime):
+                info[VehicleJournalTable.TIME_CHECK_IN] = record.check_in_time.strftime(datetime_format)
 
-st.header("Журнал")
-df = pd.DataFrame(full_events, columns=VehicleJournalTable.items())
-st.dataframe(df)
+            if isinstance(record.check_out_time, datetime):
+                info[VehicleJournalTable.TIME_CHECK_OUT] = record.check_out_time.strftime(datetime_format)
 
-st.download_button(
-   "Завантажити",
-   df.to_csv().encode('utf-8'),
-   f"events_{datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.csv",
-   "text/csv",
-   key='download-csv'
-)
+            events_df.append(info)
+
+    df = pd.DataFrame(events_df, columns=data_columns)
+
+    # sort by time
+    check_out_df_column = f"{VehicleJournalTable.TIME_CHECK_OUT}_dt"
+    df[check_out_df_column] = pd.to_datetime(df[VehicleJournalTable.TIME_CHECK_OUT])
+    df = df.sort_values(by=check_out_df_column, ascending=False)
+    df.drop(columns=check_out_df_column, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    # convert to Excel and Save to file
+    with io.BytesIO() as buffer:
+        with pd.ExcelWriter(buffer) as writer:
+            # Convert the dataframe to an XlsxWriter Excel object.
+            df.to_excel(writer, index=False, sheet_name='Журнал')
+
+            # Auto-adjust columns' width
+            for column in df:
+                column_width = max(df[column].astype(str).map(len).max(), len(column))
+                col_idx = df.columns.get_loc(column)
+                writer.sheets['Журнал'].set_column(col_idx, col_idx, column_width)
+
+        elem_name = Controls.DOWNLOAD
+        ctrl_containers[elements_indexes[elem_name]].download_button(
+            label=elem_name,
+            data=buffer.getvalue(),
+            file_name=f"events_{datetime.now().strftime('%d-%m-%Y_%H-%M-%S')}.xlsx",
+            mime="application/vnd.ms-excel",
+            disabled=len(df) <= 0
+        )
+
+    # display table
+    df.reset_index(drop=True, inplace=True)
+    st.dataframe(df[short_data_columns])
+
+main()
+
