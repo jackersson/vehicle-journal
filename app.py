45a849766d4d6b3b67b9d6e3c8474ccaa466216b
taras.lishchenko <taras.lishchenko@gmail.com>
2022-06-30 12:55:17 +0300

    latest fixes

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def format_center(text: str):
 def format_style(text: str, color: str):
     return f"<span class='highlight {color}'>{text}</span>"
 
-
+TIME_NOT_SET = "N/A"
 class VehicleJournalTable:
     ID = "№"
     VEHICLE_MODEL = "марка машини"
@@ -35,6 +35,13 @@ class VehicleJournalTable:
     RESPONSIBLE = "в чиє розпорядження"
     TIME_CHECK_OUT = "час виїзду"
     TIME_CHECK_IN = "час повернення"
+
+    @classmethod
+    def dtypes(cls):
+        return {
+            cls.ID: int,
+            cls.LICENCE_PLATE: str,
+        }
 
 
 class Controls:
@@ -136,13 +143,17 @@ def sort_by_check_out_time(df: pd.DataFrame, ascending: bool = False):
     return df
 
 # @st.cache(allow_output_mutation=True)
-def load_events(filename):
+def load_events(filename,
+                columns_name_mapping):
     events = defaultdict(VehicleLogs)
 
     try:
         df = pd.read_csv(filename)
+        df.rename(columns=columns_name_mapping, inplace = True)
+        df.set_index(VehicleJournalTable.ID)
         df = sort_by_check_out_time(df, ascending=True)
-        df.columns = df.columns.str.lower()
+        df = df.astype(VehicleJournalTable.dtypes())
+
         for _, row in df.iterrows():
             try:
                 time_check_in = datetime.strptime(str(row[VehicleJournalTable.TIME_CHECK_IN]),
@@ -159,76 +170,43 @@ def load_events(filename):
                                                                    time_check_out))
     except pd.errors.EmptyDataError as e:
         pass
-    return events
+    return events, df
 
 
-def main():
+def events_to_df(events, df):
+    events_df = []
+    for _, row in df.iterrows():
+        logs = events[row[VehicleJournalTable.LICENCE_PLATE]]
+        for record in logs:
+            info = deepcopy(row)
+            info[VehicleJournalTable.TIME_CHECK_IN] = TIME_NOT_SET
+            info[VehicleJournalTable.TIME_CHECK_OUT] = TIME_NOT_SET
+            if isinstance(record.check_in_time, datetime):
+                info[VehicleJournalTable.TIME_CHECK_IN] = record.check_in_time.strftime(datetime_format)
 
-    st.set_page_config(layout="wide")
+            if isinstance(record.check_out_time, datetime):
+                info[VehicleJournalTable.TIME_CHECK_OUT] = record.check_out_time.strftime(datetime_format)
 
-    local_css("style.css")
+            events_df.append(info)
+    return events_df
 
-    # log_file = Path(f"logs/log_{datetime.now().strftime('%d-%m-%Y')}.csv")
-    log_file = Path(f"logs/log.csv")
 
-    log_file.parent.mkdir(parents=True, exist_ok=True)
-    log_file.touch(exist_ok=True)
+class Page:
+    VEHICLES = "Наряд"
+    JOURNAL = "Журнал"
 
-    # st.header(Controls.HEADER)
+    @classmethod
+    def items(cls):
+        return [
+            cls.VEHICLES, cls.JOURNAL
+        ]
 
-    # read vehicles from file
-    uploaded_file = st.sidebar.file_uploader(Controls.UPLOAD_FILE, type="xlsx")
 
-    header = f"Наряд №0001 на {datetime.now().strftime('%d.%m.%Y')}"
-    columns_name_mapping = {}
-    if uploaded_file:
-        header = pd.read_excel(uploaded_file, usecols=[0], nrows=1).columns[0]
-        vehicles = pd.read_excel(uploaded_file,
-                                 converters={VehicleJournalTable.ID: int},
-                                 skiprows=1)
-        columns_name_mapping = {col: col.lower() for col in vehicles.columns}
-        vehicles.rename(columns=columns_name_mapping, inplace = True)
-        vehicles.set_index(VehicleJournalTable.ID)
-    else:
-        return
-
-    # st.markdown(f"<h1 style='text-align: center'> {header} </h1>", unsafe_allow_html=True)
-    st.header(header)
-
-    # load events from history (cache)
-    # if 'events' not in st.session_state:
-    events = load_events(str(log_file))
-
-    st.sidebar.markdown("---")
-
-    btn_load = st.sidebar.empty()
-
-    # clear all button
-    if st.sidebar.button(Controls.CLEAR_ALL):
-        for e in events.values():
-            e.clear()
-
-    # clear (checked-in) button
-    if st.sidebar.button(Controls.CLEAR_CHECKED_IN):
-        for e in events.values():
-            e.clear_checked_in()
-
-    skip_columns = set([VehicleJournalTable.GROUP_OF_OPERATION,
-                        VehicleJournalTable.VEHICLE_PURPOSE])
-
-    # add columns for state (`check_in`, `check_out`)
-    data_columns = list(vehicles.columns)
-    short_data_columns = [c for c in data_columns if c not in skip_columns]
-    for i, (column, control) in enumerate(
-            zip([VehicleJournalTable.TIME_CHECK_OUT,
-                VehicleJournalTable.TIME_CHECK_IN],
-                [Controls.CHECK_OUT, Controls.CHECK_IN])):
-        vehicles.insert(list(vehicles.columns).index(column),
-                        control, [bool(i)] * len(vehicles))
-
-    skip_columns.add(VehicleJournalTable.ID)
+def display_vehicles_page(events,
+                          vehicles,
+                          skip_columns,
+                          short_data_columns):
     display_columns = [c for c in vehicles.columns if c not in skip_columns]
-
     # print header of table
     sizes = [1] * len(display_columns)
     containers = st.columns(sizes)
@@ -240,7 +218,6 @@ def main():
     # print table's rows
     containers = st.columns(sizes)
     indexes = {c:i for i, c in enumerate(display_columns)}
-
     vehicles_data = vehicles[short_data_columns]
     for _, row in vehicles_data.iterrows():
         idx = row[VehicleJournalTable.LICENCE_PLATE]
@@ -263,7 +240,6 @@ def main():
                 continue
 
             text = row[col]
-
 
             if col == VehicleJournalTable.TIME_CHECK_IN:
                 text = "_"
@@ -290,38 +266,127 @@ def main():
         st.markdown("""---""")
 
 
+def main():
+
+    st.set_page_config(layout="wide")
+
+    local_css("style.css")
+
+    # log_file = Path(f"logs/log_{datetime.now().strftime('%d-%m-%Y')}.csv")
+    log_file = Path(f"logs/log.csv")
+
+    log_file.parent.mkdir(parents=True, exist_ok=True)
+    log_file.touch(exist_ok=True)
+
+    # st.header(Controls.HEADER)
+
+    # read vehicles from file
+    uploaded_file = st.sidebar.file_uploader(Controls.UPLOAD_FILE, type="xlsx")
+
+    header = f"Наряд №0001 на {datetime.now().strftime('%d.%m.%Y')}"
+    num_vehicles_total = None
+    columns_name_mapping = {}
+    if uploaded_file:
+        # header = pd.read_excel(uploaded_file, usecols=[0], nrows=1)# .columns[0]
+        columns_in_header = list(pd.read_excel(uploaded_file, nrows=1).columns)
+        valid_columns = [c for c in columns_in_header if "Unnamed" not in str(c)]
+        if len(valid_columns) == 1:
+            header = valid_columns[0]
+        elif len(valid_columns) > 2:
+            header = valid_columns[2]
+            num_vehicles_total = int(valid_columns[1])
+        else:
+            pass
+
+        vehicles = pd.read_excel(uploaded_file,
+                                 skiprows=1)
+        columns_name_mapping = {col: col.lower() for col in vehicles.columns}
+        vehicles.rename(columns=columns_name_mapping, inplace = True)
+        vehicles.set_index(VehicleJournalTable.ID)
+        vehicles = vehicles.astype(VehicleJournalTable.dtypes())
+
+        # check duplicates
+        license_plates = vehicles[VehicleJournalTable.LICENCE_PLATE]
+        duplicated = license_plates[license_plates.duplicated()]
+        if len(duplicated) > 0:
+            st.error("Таблиця містить не унікальні номерні знаки. "
+                     "Виправте, та перезавантажте таблицю, щоб продовжити")
+            st.dataframe(duplicated)
+            return
+    else:
+        return
+
+    page = st.sidebar.radio("", Page.items())
+
+
+
+    # load events from history (cache)
+    # if 'events' not in st.session_state:
+    events, events_vehicles = load_events(str(log_file),
+                                          columns_name_mapping)
+    st.sidebar.markdown("---")
+
+    btn_load = st.sidebar.empty()
+
+    # clear all button
+    if st.sidebar.button(Controls.CLEAR_ALL):
+        for e in events.values():
+            e.clear()
+            events_vehicles.drop(events_vehicles.index, inplace=True)
+
+    # clear (checked-in) button
+    if st.sidebar.button(Controls.CLEAR_CHECKED_IN):
+        for e in events.values():
+            e.clear_checked_in()
+            events_vehicles = \
+                events_vehicles[events_vehicles[VehicleJournalTable.TIME_CHECK_IN] == TIME_NOT_SET]
+
+    skip_columns = set([VehicleJournalTable.GROUP_OF_OPERATION,
+                        VehicleJournalTable.VEHICLE_PURPOSE])
+
+    # add columns for state (`check_in`, `check_out`)
+    data_columns = list(vehicles.columns)
+    short_data_columns = [c for c in data_columns if c not in skip_columns]
+    for i, (column, control) in enumerate(
+            zip([VehicleJournalTable.TIME_CHECK_OUT,
+                VehicleJournalTable.TIME_CHECK_IN],
+                [Controls.CHECK_OUT, Controls.CHECK_IN])):
+        vehicles.insert(list(vehicles.columns).index(column),
+                        control, [bool(i)] * len(vehicles))
+
+    skip_columns.add(VehicleJournalTable.ID)
+
+    if page == Page.VEHICLES:
+        # st.markdown(f"<h1 style='text-align: center'> {header} </h1>", unsafe_allow_html=True)
+        st.header(header)
+        display_vehicles_page(events, vehicles, skip_columns, short_data_columns)
+
     # convert `events` to dataframe
     events_df = []
-    for _, row in vehicles[data_columns].iterrows():
-        logs = events[row[VehicleJournalTable.LICENCE_PLATE]]
-        for record in logs:
-            info = deepcopy(row)
-            info[VehicleJournalTable.TIME_CHECK_IN] = "N/A"
-            info[VehicleJournalTable.TIME_CHECK_OUT] = "N/A"
-            if isinstance(record.check_in_time, datetime):
-                info[VehicleJournalTable.TIME_CHECK_IN] = record.check_in_time.strftime(datetime_format)
+    events_df.extend(events_to_df(events, vehicles[data_columns]))
 
-            if isinstance(record.check_out_time, datetime):
-                info[VehicleJournalTable.TIME_CHECK_OUT] = record.check_out_time.strftime(datetime_format)
-
-            events_df.append(info)
+    vehicles_license_plates = set(vehicles[VehicleJournalTable.LICENCE_PLATE])
+    events_df.extend(
+        events_to_df(
+            events,
+            events_vehicles[~events_vehicles[VehicleJournalTable.LICENCE_PLATE].isin(vehicles_license_plates)]
+        )
+    )
 
     df = pd.DataFrame(events_df, columns=data_columns)
 
     # sort by time
     df = sort_by_check_out_time(df)
     df.reset_index(drop=True, inplace=True)
+    df.rename(columns={v: k for k, v in columns_name_mapping.items()}, inplace=True)
 
     df.to_csv(str(log_file), index=False)
-
-    st.header(f"Журнал [{len(df)}]")
 
     # convert to Excel and Save to file
     with io.BytesIO() as buffer:
         with pd.ExcelWriter(buffer) as writer:
             # Convert the dataframe to an XlsxWriter Excel object.
 
-            df.rename(columns={v: k for k, v in columns_name_mapping.items()}, inplace=True)
             df.to_excel(writer, index=False, sheet_name='Журнал')
 
             # Auto-adjust columns' width
@@ -339,12 +404,30 @@ def main():
             disabled=len(df) <= 0
         )
 
+    num_checked_out = sum([len(logs) > 0 and not logs.checked_in for logs in events.values()])
+    num_vehicles_total = num_vehicles_total or len(vehicles)
+
+    st.sidebar.subheader("Кількість")
+    vehicle_counts = {
+        "За списком": num_vehicles_total,
+        "На виїзді": num_checked_out,
+        "В наявності": num_vehicles_total - num_checked_out,
+    }
+    columns = st.sidebar.columns(2)
+    for (k, v) in vehicle_counts.items():
+        columns[0].text(k)
+        columns[1].text(v)
+
+
     # display table
-    inv_columns_name_mapping = {v: k for k, v in columns_name_mapping.items()}
-    df.reset_index(drop=True, inplace=True)
-    for c in df.columns:
-        df[c] = df[c].astype(str)
-    st.dataframe(df[[inv_columns_name_mapping[c] for c in short_data_columns]])
+    if page == Page.JOURNAL:
+        st.header(f"Журнал [{len(df)}]")
+
+        inv_columns_name_mapping = {v: k for k, v in columns_name_mapping.items()}
+        df.reset_index(drop=True, inplace=True)
+        for c in df.columns:
+            df[c] = df[c].astype(str)
+        st.dataframe(df[[inv_columns_name_mapping[c] for c in short_data_columns]])
 
 main()
 
